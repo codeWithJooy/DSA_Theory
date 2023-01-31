cae2ef63515927a2654bb8cdec0935b668925d04
Abhishek Hazra <abhishek.hazra@awesomesuite.com>
2023-01-31 17:51:09 +0530

    Add Node at End Done

--- LinkedList/SingleLLL/singleLL.js
+++ LinkedList/SingleLLL/singleLL.js
@@ -9,10 +9,20 @@ class LinkedList {
   constructor() {
     this.head = null;
   }
+  /* Add Data at the Beginning */
   addAtStart(data) {
     let newNode = new Node(data);
     newNode.next = this.head;
     this.head = newNode;
+  }
+  /* Add Data at The End */
+  addAtEnd(data) {
+    let newNode = new Node(data);
+    let current = this.head;
+    while (current.next != null) {
+      current = current.next;
+    }
+    current.next = newNode;
   }
 
   printLinkedList() {
@@ -25,6 +35,8 @@ class LinkedList {
 }
 
 let head = new LinkedList();
-head.addAtStart(10);
 head.addAtStart(20);
+head.addAtStart(10);
+head.addAtEnd(30);
+head.addAtEnd(40);
 head.printLinkedList();
